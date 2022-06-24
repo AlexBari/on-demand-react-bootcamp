94d60b6dde38a40301b9bee5ab95fa7f9a122791
AlexBari <69476141+AlexBari@users.noreply.github.com>
2022-06-24 12:04:20 -0500

    Mmmm

--- src/common/header.js
+++ src/common/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import SearchBar from '../components/searchBar/searchBar';
-import ShoppingCar from '../components/shoppinCcar/shoppingCar'
+import ShoppingCar from '../components/shoppinCar/shoppingCar'
 import SidebarNav from '../components/sidebarNav/sidebarNav'
 import NavMenu from './navMenu';
 import {
